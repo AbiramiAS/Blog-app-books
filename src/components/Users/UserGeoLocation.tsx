25d7989ce63a6257e27e74fc4f718aaf0dcdc7d5
AbiramiAS <abiramishanmugamvkl@gmail.com>
2025-11-07 12:17:31 +0530

    User geo location changes

--- src/components/Users/UserGeoLocation.tsx
+++ src/components/Users/UserGeoLocation.tsx
@@ -4,27 +4,21 @@ import "maplibre-gl/dist/maplibre-gl.css";
 import type { Props } from "../../types/types";
 
 const UserGeoLocation: React.FC<Props> = ({ userLongitude, userLatitude }) => {
-  console.log("UserGeoLocation props:", { userLongitude, userLatitude });
-  console.log("UserGeoLocation coordinates:", {
-    longitude: Number(userLongitude.toPrecision(3)),
-    latitude: Number(userLatitude.toPrecision(3)),
-  }); 
+  const longitude = userLongitude || 76.93275456824303;
+  const latitude = userLatitude || 11.02878910296039;
+
   return (
     <div>
       <Map
         initialViewState={{
-          longitude: 76.93275456824303, // Number(userLongitude.toPrecision(3)), // -37.3, //-122.4
-          latitude: 11.02878910296039, //Number(userLatitude.toPrecision(3)), // 81.1, //37.8
+          longitude: longitude,
+          latitude: latitude,
           zoom: 14,
         }}
         style={{ width: 600, height: 450 }}
         mapStyle="https://api.maptiler.com/maps/streets/style.json?key=5EOLB4Fbz0492dmM4UPS"
       >
-        <Marker
-          longitude={76.93275456824303}
-          latitude={11.02878910296039}
-          anchor="bottom"
-        />
+        <Marker longitude={longitude} latitude={latitude} anchor="bottom" />
       </Map>
     </div>
   );
