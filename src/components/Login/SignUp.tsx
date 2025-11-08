8277180b7980bdbf39dc74e86f18f7ce83afd4fa
AbiramiAS <abiramishanmugamvkl@gmail.com>
2025-11-08 06:06:27 +0530

    Changes for including Sign up page

--- src/components/Login/SignUp.tsx
+++ src/components/Login/SignUp.tsx
@@ -1,14 +1,14 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-const Login = () => {
-  const [username, setUsername] = useState("");
+const SignUp = () => {
+  const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const navigate = useNavigate();
 
-  const handleLoginSubmit = (e: React.FormEvent) => {
+  const handleSignupSubmit = (e: React.FormEvent) => {
     e.preventDefault();
-    if (!username || !password) return;
+    if (!email || !password) return;
     navigate("/home");
   };
 
@@ -16,21 +16,21 @@ const Login = () => {
     <div className="flex min-h-screen bg-gray-800 items-center justify-center py-8">
       <div className="w-full max-w-md">
         <div className="mb-6 sm:mb-8 text-center">
-          <p className="mt-2 text-4xl text-gray-100">Books Blog Login</p>
+          <p className="mt-2 text-4xl text-gray-100">Sign Up to Books Blog</p>
         </div>
 
         <div className="rounded-2xl border border-gray-800 bg-gray-900 p-5 sm:p-6">
-          <form onSubmit={handleLoginSubmit}>
+          <form onSubmit={handleSignupSubmit}>
             <div>
               <label className="block text-sm font-medium text-gray-300 pb-2">
-                Username
+                Email ID
               </label>
               <input
-                id="username"
-                name="username"
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
-                placeholder="Your Username"
+                id="email"
+                name="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                placeholder="Your Email ID"
                 className="mt-1 block w-full rounded-xl border bg-gray-900 px-3 py-2 text-gray-100"
               />
             </div>
@@ -50,7 +50,7 @@ const Login = () => {
             />
             <button
               type="submit"
-              disabled={!username || !password}
+              disabled={!email || !password}
               className="inline-flex w-full items-center justify-center mt-4 rounded-xl bg-indigo-500 px-4 py-2.5 text-white cursor-pointer disabled-button"
             >
               Submit
@@ -62,4 +62,4 @@ const Login = () => {
   );
 };
 
-export default Login;
+export default SignUp;
--- src/routes/RouterFunction.tsx
+++ src/routes/RouterFunction.tsx
@@ -1,9 +1,10 @@
 import { Routes, Route } from "react-router-dom";
-import BooksDisplay from "../components/Books/BooksDisplay";
-import UserList from "../components/Users/UserList";
-import UserItem from "../components/Users/UserItem";
-import Login from "../components/Login/Login";
-import Layout from "../components/Header/Layout";
+import BooksDisplay from "@/components/Books/BooksDisplay";
+import UserList from "@/components/Users/UserList";
+import UserItem from "@/components/Users/UserItem";
+import Login from "@/components/Login/Login";
+import Layout from "@/components/Header/Layout";
+import SignUp from "@/components/Login/SignUp";
 
 function RouterFunction() {
   return (
@@ -21,6 +22,9 @@ function RouterFunction() {
       <Route path="/login">
         <Route index element={<Login />} />
         <Route path="/login/home" element={<BooksDisplay />} />
+      </Route>
+      <Route path="/signup">
+        <Route index element={<SignUp />} />
       </Route>
     </Routes>
   );
